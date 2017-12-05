418365aaf051ff52ed0a683c15e35e5a7ba5854c
Sokrates Potamias <sokratespot@gmail.com>
2017-12-05 23:12:51 +0200

    Version0.1

---
+++ Code/test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Php says hi!</title>
+</head>
+<body>
+
+<b>
+	<?php 
+	print "Hello, Wolrd!";
+	?>
+</b>
+
+<li>mpla</li>
+<li>mpla</li>
+<a href="https://www.ika.gr" target="_blank">OLD!</a>
+
+</body>
+</html>
