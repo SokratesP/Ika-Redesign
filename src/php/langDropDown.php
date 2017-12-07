51985b93bd7e8a550284f48aec0257ad076cfa1a
Sokrates Potamias <sokratespot@gmail.com>
2017-12-07 17:07:04 +0200

    version0.02

---
+++ src/php/langDropDown.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="../css/langDropDown.css">
+    <meta charset="utf-8">
+    <title>LanguageDropDown</title>
+  </head>
+  <body>
+    <ul class="languagepicker roundborders large">
+      <a href="#gr"><li><img src="../img/flags/greece.png"/>Greek</li></a>
+      <a href="#en"><li><img src="../img/flags/american.png"/>English</li></a>
+      <a href="#de"><li><img src="../img/flags/germany.png"/>German</li></a>
+      <a href="#fr"><li><img src="../img/flags/france.png"/>Français</li></a>
+    </ul>
+  </body>
+</html>
