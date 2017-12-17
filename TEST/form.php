87d2b26a85f6b69b29be22924e0d611aab3513f0
Sokrates Potamias <sokratespot@gmail.com>
2017-12-17 20:04:11 +0200

    v0.3

--- TEST/form.php
+++ TEST/form.php
@@ -3,11 +3,26 @@
 <?php
   include("config.php");
   session_start();
+  $error = " ";
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $myusername = mysqli_real_escape_string($db,$_POST['username']);
     $mypassword = mysqli_real_escape_string($db,$_POST['password']);
 
-    $sql = "SELECT id FROM myusers WHERE "
+    $sql = "SELECT id FROM myusers WHERE username = '$myusername' and passcode = '$mypassword' ";
+    $result = mysqli_query($db,$sql);
+    #$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    #$active = $row['active'];
+
+    $count  = mysqli_num_rows($result);
+
+    if ($count == 1) {
+      #sesssion_register("myusername");
+      $_SESSION['login_user'] = $myusername;
+
+      header("location: welcome.php");
+    }else{
+      $error = "Your login Name or Password is invalid";
+    }
 
   }
 
@@ -18,11 +33,19 @@
     <meta charset="utf-8">
     <title>My login page!</title>
   </head>
+
   <body>
-    <form action="" method="post">
-      UserName : <input type="text" name="username"><br>
-      Passwrod : <input type="password" name="password"><br>
-      <input type="submit" value=" Submit"><br>
-    </form>
+    <div>
+
+      <form action="" method="post">
+        <label>UserName :</label><input type = "text" name = "username"/><br/><br/>
+        <label>Passwrod :</label> <input type="password" name="password"/><br/><br/>
+        <input type="submit" value=" Submit"/><br/>
+      </form>
+
+      <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
+
+    </div>
+
   </body>
 </html>
